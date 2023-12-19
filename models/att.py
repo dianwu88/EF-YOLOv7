07086ad19720bfdb36c61d0a225b1613f451c85c
dianwu88 <154330979+dianwu88@users.noreply.github.com>
2023-12-19 19:56:10 +0800

    Add files via upload

---
+++ models/att.py
@@ -0,0 +1,369 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.nn import Softmax
+from torch.nn import init
+from torch.nn.parameter import Parameter
+
+
+class CBAMLayer(nn.Module):
+    def __init__(self, channel, reduction=16, spatial_kernel=7):
+        super(CBAMLayer, self).__init__()
+
+        # channel attention 压缩H,W为1
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        # shared MLP
+        self.mlp = nn.Sequential(
+            # Conv2d比Linear方便操作
+            # nn.Linear(channel, channel // reduction, bias=False)
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            # inplace=True直接替换，节省内存
+            nn.ReLU(inplace=True),
+            # nn.Linear(channel // reduction, channel,bias=False)
+            nn.Conv2d(channel // reduction, channel, 1, bias=False)
+        )
+
+        # spatial attention
+        self.conv = nn.Conv2d(2, 1, kernel_size=spatial_kernel,
+                              padding=spatial_kernel // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        max_out = self.mlp(self.max_pool(x))
+        avg_out = self.mlp(self.avg_pool(x))
+        channel_out = self.sigmoid(max_out + avg_out)
+        x = channel_out * x
+
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        spatial_out = self.sigmoid(self.conv(torch.cat([max_out, avg_out], dim=1)))
+        x = spatial_out * x
+        return x
+
+def INF(B,H,W):
+     return -torch.diag(torch.tensor(float("inf")).repeat(H),0).unsqueeze(0).repeat(B*W,1,1)
+
+class CrissCrossAttention(nn.Module):
+    """ Criss-Cross Attention Module"""
+
+    def __init__(self, in_dim):
+        super(CrissCrossAttention, self).__init__()
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.softmax = Softmax(dim=3)
+        self.INF = INF
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        m_batchsize, _, height, width = x.size()
+        device = x.device
+        proj_query = self.query_conv(x)
+        proj_query_H = proj_query.permute(0, 3, 1, 2).contiguous().view(m_batchsize * width, -1, height).permute(0, 2, 1)
+        proj_query_W = proj_query.permute(0, 2, 1, 3).contiguous().view(m_batchsize * height, -1, width).permute(0, 2, 1)
+        proj_key = self.key_conv(x)
+        proj_key_H = proj_key.permute(0, 3, 1, 2).contiguous().view(m_batchsize * width, -1, height)
+        proj_key_W = proj_key.permute(0, 2, 1, 3).contiguous().view(m_batchsize * height, -1, width)
+        proj_value = self.value_conv(x)
+        proj_value_H = proj_value.permute(0, 3, 1, 2).contiguous().view(m_batchsize * width, -1, height)
+        proj_value_W = proj_value.permute(0, 2, 1, 3).contiguous().view(m_batchsize * height, -1, width)
+
+        tmp_inf = self.INF(m_batchsize, height, width).to(device)
+        energy_H = (torch.bmm(proj_query_H, proj_key_H).to(device) + tmp_inf).view(m_batchsize, width, height,height).permute(0, 2, 1, 3)
+
+        # energy_H = (torch.bmm(proj_query_H, proj_key_H) + self.INF(m_batchsize, height, width)).view(m_batchsize, width,height,height).permute(0,2,1,3)
+        energy_W=torch.bmm(proj_query_W,proj_key_W).view(m_batchsize,height,width,width)
+        concate = self.softmax(torch.cat([energy_H, energy_W], 3))
+
+        att_H = concate[:, :, :, 0:height].permute(0, 2, 1, 3).contiguous().view(m_batchsize * width, height, height)
+        # print(concate)
+        # print(att_H)
+        att_W = concate[:, :, :, height:height + width].contiguous().view(m_batchsize * height, width, width)
+        out_H = torch.bmm(proj_value_H, att_H.permute(0, 2, 1)).view(m_batchsize, width, -1, height).permute(0, 2, 3, 1)
+        out_W = torch.bmm(proj_value_W, att_W.permute(0, 2, 1)).view(m_batchsize, height, -1, width).permute(0, 2, 1, 3)
+        # print(out_H.size(),out_W.size())
+        return self.gamma * (out_H + out_W) + x
+
+class RFAconv(nn.Module):
+    """ Criss-Cross Attention Module"""
+
+    def __init__(self, in_dim):
+        super(RFAconv, self).__init__()
+        self.h_pool = nn.AdaptiveAvgPool2d((None,1))
+        self.w_pool = nn.AdaptiveAvgPool2d((1,None))
+        self.conv = nn.Sequential(
+                                nn.Conv2d(in_dim // 16,out_channels=in_dim,kernel_size=1,bias=False),
+                                nn.Softmax(1)
+                                # nn.Conv2d(in_dim // 16,out_channels=in_dim,kernel_size=1,bias=False),
+                            )
+        self.conv_block = nn.Sequential(
+                                nn.Conv2d(in_dim,out_channels=in_dim // 16,kernel_size=1,bias=False),
+                                nn.ReLU()
+                                # nn.Conv2d(in_dim // 16,out_channels=in_dim,kernel_size=1,bias=False),
+                            )
+        self.group_conv1 = nn.Conv2d(in_channels=in_dim,out_channels=in_dim,kernel_size=1,groups=in_dim)
+        self.group_conv3 = nn.Conv2d(in_channels=in_dim // 16, out_channels=in_dim // 16, kernel_size=3, groups=4,padding=1)
+        self.linear = nn.Linear(1,1)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(1)
+        self.sigmoid = nn.Sigmoid()
+
+        self.in_dim = in_dim
+        self.convA = nn.Conv2d(in_dim, in_dim , kernel_size=1)
+        self.convB = nn.Conv2d(in_dim, in_dim , kernel_size=1)
+        self.convV = nn.Conv2d(in_dim, in_dim , kernel_size=1)
+
+
+    def forward(self,x):
+        b,c,h,w = x.size()
+        x_h = self.conv_block(self.h_pool(x))
+        x_w = self.conv_block(self.w_pool(x)).permute(0,1,3,2)
+        x1 = self.group_conv3(torch.cat([x_h, x_w], dim=2))
+        x_h,x_w = torch.split(x1,[h,w],dim=2)
+        x_h = self.conv(x_h)
+        x_w = self.conv(x_w)
+        x2 = x_h * x_w.permute(0,1,3,2)
+        x3 = torch.mean(x2,dim=1, keepdim=True)
+        out = x3 * x
+        return out
+
+class DiffAttentionModule(nn.Module):
+    def __init__(self, num_feature, in_transform, diff_ratio, out_transform, aggregate=True):
+        super(DiffAttentionModule, self).__init__()
+        self.num_feature = num_feature
+        self.in_transform = in_transform
+        self.diff_ratio = diff_ratio
+        self.out_transform = out_transform
+        self.aggregate = aggregate
+        if self.aggregate:
+            self.conv1 = nn.Conv1d(in_channels=3, out_channels=1, kernel_size=1, padding='same')
+        self.fc1 = nn.Linear(self.num_feature, self.num_feature // self.diff_ratio, bias=False)
+        self.fc2 = nn.Linear(self.num_feature // self.diff_ratio, self.num_feature, bias=False)
+
+    def forward(self, x, y, keep_dim=True):
+        # Make diff with input transform.
+        diff = x - y
+        if self.in_transform == 'abs':
+            diff = torch.abs(diff)
+        elif self.in_transform == 'square':
+            diff = torch.square(diff)
+        else:
+            pass
+        # Aggregate.
+        if self.aggregate:
+            feature = torch.stack((diff, x, y), 1)
+            feature = self.conv1(feature)
+            feature = feature.view(feature.shape[0], -1)
+        else:
+            feature = diff
+        # Calculate attention.
+        diff_attention = F.relu(self.fc1(feature))
+        diff_attention = self.fc2(diff_attention)
+        # Transform output.
+        if self.out_transform == 'sigmoid':
+            diff_attention = torch.sigmoid(diff_attention)
+        else:
+            pass
+        # Output result.
+        if keep_dim:
+            x_feature = x.mul(diff_attention)
+            y_feature = y.mul(diff_attention)
+            return x_feature, y_feature
+        else:
+            return
+
+class Transcam(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+
+    def forward(self, x):
+        B, C, N,A = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+
+class ShuffleAttention(nn.Module):
+
+    def __init__(self, channel=512,reduction=16,G=8):
+        super().__init__()
+        self.G=G
+        self.channel=channel
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
+        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
+        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
+        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
+        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
+        self.sigmoid=nn.Sigmoid()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+
+    @staticmethod
+    def channel_shuffle(x, groups):
+        b, c, h, w = x.shape
+        x = x.reshape(b, groups, -1, h, w)
+        x = x.permute(0, 2, 1, 3, 4)
+
+        # flatten
+        x = x.reshape(b, -1, h, w)
+
+        return x
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        #group into subfeatures
+        x=x.view(b*self.G,-1,h,w) #bs*G,c//G,h,w
+
+        #channel_split
+        x_0,x_1=x.chunk(2,dim=1) #bs*G,c//(2*G),h,w
+
+        #channel attention
+        x_channel=self.avg_pool(x_0) #bs*G,c//(2*G),1,1
+        x_channel=self.cweight*x_channel+self.cbias #bs*G,c//(2*G),1,1
+        x_channel=x_0*self.sigmoid(x_channel)
+
+        #spatial attention
+        x_spatial=self.gn(x_1) #bs*G,c//(2*G),h,w
+        x_spatial=self.sweight*x_spatial+self.sbias #bs*G,c//(2*G),h,w
+        x_spatial=x_1*self.sigmoid(x_spatial) #bs*G,c//(2*G),h,w
+
+        # concatenate along channel axis
+        out=torch.cat([x_channel,x_spatial],dim=1)  #bs*G,c//G,h,w
+        out=out.contiguous().view(b,-1,h,w)
+
+        # channel shuffle
+        out = self.channel_shuffle(out, 2)
+        return out
+
+from torch.nn import init
+
+
+class DoubleAttention(nn.Module):
+    """
+        A2-Nets: Double Attention Networks. NIPS 2018
+    """
+    def __init__(self, in_channels, c_m, c_n, reconstruct=True):
+        super().__init__()
+        self.in_channels = in_channels
+        self.reconstruct = reconstruct
+        self.c_m = c_m
+        self.c_n = c_n
+        self.convA = nn.Conv2d(in_channels, c_m, 1)
+        self.convB = nn.Conv2d(in_channels, c_n, 1)
+        self.convV = nn.Conv2d(in_channels, c_n, 1)
+        if self.reconstruct:
+            self.conv_reconstruct = nn.Conv2d(c_m, in_channels, kernel_size=1)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        assert c == self.in_channels
+        A = self.convA(x)  # b,c_m,h,w
+        B = self.convB(x)  # b,c_n,h,w
+        V = self.convV(x)  # b,c_n,h,w
+        tmpA = A.view(b, self.c_m, -1)
+        attention_maps = F.softmax(B.view(b, self.c_n, -1), dim=1)
+        attention_vectors = F.softmax(V.view(b, self.c_n, -1), dim=1)
+        # step 1: feature gating
+        global_descriptors = torch.bmm(tmpA, attention_maps.permute(0, 2, 1))  # b.c_m,c_n
+        # step 2: feature distribution
+        tmpZ = global_descriptors.matmul(attention_vectors)  # b,c_m,h*w
+        tmpZ = tmpZ.view(b, self.c_m, h, w)  # b,c_m,h,w
+        if self.reconstruct:
+            tmpZ = self.conv_reconstruct(tmpZ)
+
+        return tmpZ
+
+class CoTAttention(nn.Module):
+
+    def __init__(self, dim=512, kernel_size=3):
+        super().__init__()
+        self.dim = dim
+        self.kernel_size = kernel_size
+
+        self.key_embed = nn.Sequential(
+            nn.Conv2d(dim, dim, kernel_size=kernel_size, padding=kernel_size // 2, groups=4, bias=False),
+            nn.BatchNorm2d(dim),
+            nn.ReLU()
+        )
+        self.value_embed = nn.Sequential(
+            nn.Conv2d(dim, dim, 1, bias=False),
+            nn.BatchNorm2d(dim)
+        )
+
+        factor = 4
+        self.attention_embed = nn.Sequential(
+            nn.Conv2d(2 * dim, 2 * dim // factor, 1, bias=False),
+            nn.BatchNorm2d(2 * dim // factor),
+            nn.ReLU(),
+            nn.Conv2d(2 * dim // factor, kernel_size * kernel_size * dim, 1)
+        )
+
+    def forward(self, x):
+        bs, c, h, w = x.shape
+        k1 = self.key_embed(x)  # bs,c,h,w
+        v = self.value_embed(x).view(bs, c, -1)  # bs,c,h,w
+
+        y = torch.cat([k1, x], dim=1)  # bs,2c,h,w
+        att = self.attention_embed(y)  # bs,c*k*k,h,w
+        att = att.reshape(bs, c, self.kernel_size * self.kernel_size, h, w)
+        att = att.mean(2, keepdim=False).view(bs, c, -1)  # bs,c,h*w
+        k2 = F.softmax(att, dim=-1) * v
+        k2 = k2.view(bs, c, h, w)
+
+        return k1 + k2
+
+
+
+
+
